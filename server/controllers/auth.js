8d564cf21a4c310883851c50c53b1a685944988e
Nurbek <nurbek.itu@gmail.com>
2022-12-21 01:53:59 +0600

    fixed error just one errrorgit add .git add .git add .git add .git add .git add .

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -41,6 +41,7 @@ export const login = async (req, res) => {
       { expiresIn: '90d' }
     );
     const { password, ...others } = user._doc;
+    res.status(200).json({ token, others });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
